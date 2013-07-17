00a10f713936bb028bc81edce044c6a72c53c62e
Nick Asteriadis <nickast@gmail.com>
2013-07-18 02:18:18 +0300

    Adding the plugin main file, the licence and the readme.txt form the wp repository

---
+++ easy-digital-downloads-delete-purchases.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads - Delete Purchases
+Plugin URI: http://www.nasteriadis.gr/easy-digital-downloads
+Description: This plugin enables the user to delete previous transactions both partially and completely.
+Version: 1.0
+Author: Nick C. Asteriadis
+Author URI: http://www.nasteriadis.gr/
+Depends: Easy Digital Downloads
+License:
+
+  Copyright 2013 nickast@gmail.com
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Easy Digital Downloads - Delete Purchases ===
+Contributors: Nick C. Asteriadis
+Donate link: http://nasteriadis.gr/delete-purchases
+Tags: easy digital downloads, delete transaction, delete purchases
+Requires at least: 3.5
+Tested up to: 3.5.2
+Stable tag: 3.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Just another contact form plugin. Simple but flexible.
+
+== Description ==
+
+DESCRIPTION GOES HERE
+
+== Installation ==
+
+INSTALLATION INFO GOES HERE
+
+== Frequently Asked Questions ==
+
+FAQ QUESTIONS HERE
+
+== Screenshots ==
+
+1. screenshot-1.png 
+
+== Changelog ==
+
+= 1.0 =
+
+Initial Release
