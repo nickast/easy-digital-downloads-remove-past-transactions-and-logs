de4383076e0835d4d7204556cd87d85876fec972
= <=>
2013-07-19 17:57:45 +0300

    Initial commit containing the licence, readme.txt and the main file of the plugin

---
+++ easy-digital-downloads-remove-purchases.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads - Remove Purchases
+Plugin URI: http://www.nasteriadis.gr/easy-digital-downloads-remove-purchases
+Description: This plugin is an extension of the easy digital downloads and enables the user to delete transactions stores in the db both completely or partially.
+Version: 1.0
+Author: Nick C. Asteriadis
+Author URI: http://www.nasteriadis.gr/
+Depends: Easy Digital Downloads
+License:
+
+  Copyright 2013 (nickast@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Easy Digital Downloads - Remove Purchases ===
+Contributors: Nick C. Asteriadis
+Donate link: http://www.nasteriadis.gr/easy-digital-downloads-remove-purchases
+Tags: easy digital downlaod, remove transactions, remove purchases
+Requires at least: 3.5
+Tested up to: 3.5.2
+Stable tag: 3.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Just another contact form plugin. Simple but flexible.
+
+== Description ==
+
+DESCRIPTION GOES HERE
+
+== Installation ==
+
+INSTALLATION INFO GOES HERE
+
+== Frequently Asked Questions ==
+
+FAQ QUESTIONS HERE
+
+== Screenshots ==
+
+1. screenshot-1.png 
+
+== Changelog ==
+
+= 1.0 =
+
+Initial Release
